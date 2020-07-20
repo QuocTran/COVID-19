fe86b3da28582344bb3073142511a408bcb510a1
qtran <ttquoc@gmail.com>
2020-07-20 10:37:11 -0700

    Improved SEIR model

--- prognosis/SEIR.py
+++ prognosis/SEIR.py
@@ -24,10 +24,11 @@ def SEIR(y, t, logbeta, logkappa, loggamma):
                      kappa*y[1] - gamma*y[2], 
                      gamma*y[2]])
     
-def minimization(y0, t, I, niter = 1):
+def minimization(y0, t, C, niter = 1):
     
     def fit_odeint(t, logbeta, logkappa, loggamma):
-        return odeint(SEIR, y0, t, args=(logbeta, logkappa, loggamma))[:,2]
+        out_odeint = odeint(SEIR, y0, t, args=(logbeta, logkappa, loggamma))
+        return out_odeint[:,2]+out_odeint[:,3]
     
     best = np.inf
     res = (0, 0, 0)
@@ -36,13 +37,13 @@ def minimization(y0, t, I, niter = 1):
         init_logkappa = 0.5 * np.random.randn()
         init_loggamma = 0.5 * np.random.randn()
         try:
-            popt, pcov = curve_fit(fit_odeint, t, I,
+            popt, pcov = curve_fit(fit_odeint, t, C,
                                    p0=np.asarray([init_logbeta,init_logkappa,init_loggamma]),
                                    maxfev=5000)
         except RuntimeError:
             print("Error - curve_fit failed")   
         fitted = fit_odeint(t, *popt)
-        value = np.sum((fitted - I)**2)
+        value = np.sum((fitted - C)**2)
         if (value < best): 
             res = popt
             best = value
